3ca597cbecb610b56cd6ef931cb21256eaa7bf5c
marcel <demdike12@gmail.com>
2026-03-30 16:54:35 -0500

    TDD: Test Validar fecha cita(funciona el test)

--- src/main/java/T1/RegistroCita.java
+++ src/main/java/T1/RegistroCita.java
@@ -7,6 +7,9 @@ public class RegistroCita {
     private List<Cita> citas = new ArrayList<>();
 
     public String registrarCita(Cita cita) {
+        if (cita.getCodigo().isEmpty() || cita.getNombre().isEmpty()) {
+            return "Debe ingresar todos los datos requeridos";
+        }
         if (cita.getCodigo() == null || !cita.getCodigo().matches("^C\\d{3}$")) {
             return "Ingrese un código de cita válido";
         }
